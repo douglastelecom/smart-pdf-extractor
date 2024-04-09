34ed8ae3ac2dcc922b654d6354148a72a8d6194e
Douglas F <douglastelec@gmail.com>
2024-04-09 11:04:16 -0300

    criando meu primeiro completion

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, {Request, Response, NextFunction} from 'express';
-import {createServer} from 'http';
+import express, { Request, Response, NextFunction } from 'express';
+import { createServer } from 'http';
 import "dotenv/config";
 import { DataExtractedController } from 'controller/dataExtractedController';
+import OpenAI from 'openai';
 
 const app = express();
 
@@ -9,18 +10,23 @@ const server = createServer(app);
 
 const port = process.env.PORT;
 
+const openai = new OpenAI();
+
 app.use(express.json())
- 
+
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
-app.get("/", (req: Request, res: Response)=>{
-    console.log(req.body)
-    res.send("Hello World!");
+app.get("/", async (req: Request, res: Response) => {
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a helpful assistant." }],
+        model: "gpt-3.5-turbo",
+    });
+    res.send(completion.choices[0]);
 })
 
-app.post("/", async (req: Request, res: Response)=>{
+app.post("/", async (req: Request, res: Response) => {
     const dataExtractedController = new DataExtractedController()
     res.send(await dataExtractedController.post(req, res));
 })
