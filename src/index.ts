87f04faa7b61a9978fb209516c349cc85dc71c8e
Douglas F <douglastelec@gmail.com>
2024-04-10 14:57:35 -0300

    sadf

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,13 @@ import "dotenv/config";
 import { DataExtractedController } from 'controller/dataExtractedController';
 import OpenAI from 'openai';
 import fs from 'fs';
+import { empty } from '@prisma/client/runtime/library';
 
 const app = express();
-
 const server = createServer(app);
-
 const port = process.env.PORT;
 
 const openai = new OpenAI();
-
 
 app.use(express.json())
 
@@ -20,32 +18,31 @@ server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 
-app.get("/completion", async (req: Request, res: Response) => {
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "Você será meu assistente" },
-        { role: "user", content: req.body.question }],
-        model: "gpt-3.5-turbo",
-    });
-    res.send(completion.choices[0].message.content);
-})
-
-app.post("/", async (req: Request, res: Response) => {
-    const dataExtractedController = new DataExtractedController()
-    res.send(await dataExtractedController.post(req, res));
-})
-
 app.post("/file", async (req: Request, res: Response) => {
-    const filegpt = await openai.files.create({ file: fs.createReadStream('src/assets/articles/corona.pdf'), purpose: 'assistants' });
+    const filegpt = await openai.files.create({ file: fs.createReadStream(req.body.path), purpose: req.body.purpose });
     console.log(filegpt)
 })
 
 app.post("/assistant", async (req: Request, res: Response) => {
-    const assistant = await openai.beta.assistants.create({
-        name: "pdf-smart-extractor",
-        description: "Você será minha ferramenta para extrair informações de arquivos pdf e retorná-los em formato json.",
-        model: "gpt-3.5-turbo",
-        tools: [{"type": "retrieval"}],
-        file_ids: ["file-3iAxjWsbxZay5c82c7jWI9lF"]
-      });
-      res.send(assistant)
+    const assistant = await openai.beta.assistants.create(req.body);
+    res.send(assistant)
 })
+
+app.post("/thread", async (req: Request, res: Response) => {
+    const run = await openai.beta.threads.createAndRunPoll({
+        assistant_id: req.body.assistant_id,
+        thread: {
+            messages: req.body.messages
+    }});
+    if (run.status === 'completed') {
+        const messages: any = await openai.beta.threads.messages.list(
+            run.thread_id
+        );
+        console.log("foi")
+        res.send(messages.data[0].content[0].text.value)
+    } else {
+        console.log("não foi")
+        res.send(run.status)
+    }
+})
+
